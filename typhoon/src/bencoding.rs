9831aaa42af18fbe4a25707d90eab73358924810
Lúcás Meier <cronokirby@gmail.com>
2019-10-19 00:16:22 +0200

    Add parsing of bencoding integers

--- typhoon/src/bencoding.rs
+++ typhoon/src/bencoding.rs
@@ -1,4 +1,24 @@
+extern crate nom;
+use nom::{
+    branch::alt,
+    bytes::complete::take_while1,
+    character::{complete::char, is_digit},
+    combinator::{map, map_res},
+    sequence::{preceded, terminated},
+    IResult,
+};
+
 use std::collections::HashMap;
+use std::str;
+
+/// Represents an error that occurs while parsing bencoded data.
+///
+/// For now, this isn't very useful, and just contains a formatted string
+/// produced by our parsing framework. We could produce more useful input by
+/// manually inspecting errors to figure out how exactly things failed, or if
+/// we could re run the parser on the data with invalid UTF-8 sections stripped.
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct BencodingError(String);
 
 /// Represents a general data structure expressable with "bencoding"
 ///
@@ -35,4 +55,64 @@ pub enum Bencoding {
     /// The keys of this map are subject to the same caveats as byte sequence elements in this
     /// enum. In practice though, non UTF-8 map keys don't seem to appear.
     Dict(HashMap<Box<[u8]>, Bencoding>),
+}
+
+impl Bencoding {
+    fn parse(input: &[u8]) -> Result<Bencoding, BencodingError> {
+        match bencoding(input) {
+            Ok((_, res)) => Ok(res),
+            Err(e) => {
+                let msg = match e {
+                    nom::Err::Error((raw, kind)) | nom::Err::Failure((raw, kind)) => {
+                        let lossy = String::from_utf8_lossy(raw);
+                        format!("lossy: {}, raw: {:?}, kind: {:?}", lossy, raw, kind)
+                    }
+                    other => format!("{:?}", other),
+                };
+                Err(BencodingError(msg))
+            }
+        }
+    }
+}
+
+fn bencoding(input: &[u8]) -> IResult<&[u8], Bencoding> {
+    // The return value of this function is always positive
+    fn int_digits(input: &[u8]) -> IResult<&[u8], i64> {
+        let digits = take_while1(is_digit);
+        // We don't need to check, since we've parse only ASCII digits
+        let get_str = map(digits, |bytes| unsafe { str::from_utf8_unchecked(bytes) });
+        // This shouldn't ever fail, once again because of the ASCII digits
+        map_res(get_str, |string| i64::from_str_radix(string, 10))(input)
+    }
+
+    fn signed_int(input: &[u8]) -> IResult<&[u8], i64> {
+        let negative = map(preceded(char('-'), int_digits), |i| -i);
+        alt((negative, int_digits))(input)
+    }
+
+    fn int(input: &[u8]) -> IResult<&[u8], Bencoding> {
+        let wrapped = terminated(preceded(char('i'), signed_int), char('e'));
+        map(wrapped, Bencoding::Int)(input)
+    }
+
+    int(input)
+}
+
+#[cfg(test)]
+mod test {
+    use super::Bencoding;
+
+    #[test]
+    fn parsing_positive_integers_works() {
+        let input = b"i123e";
+        let output = Bencoding::parse(input);
+        assert_eq!(Ok(Bencoding::Int(123)), output);
+    }
+
+    #[test]
+    fn parsing_negative_integers_works() {
+        let input = b"i-111e";
+        let output = Bencoding::parse(input);
+        assert_eq!(Ok(Bencoding::Int(-111)), output);
+    }
 }
