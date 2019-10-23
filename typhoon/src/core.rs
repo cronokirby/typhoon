1eec3504021f5e62d34208101a43dae7e9691e18
Lúcás Meier <cronokirby@gmail.com>
2019-10-23 20:31:36 +0200

    WIP: parsing tracker files

--- typhoon/src/core.rs
+++ typhoon/src/core.rs
@@ -3,13 +3,14 @@
 //! This includes definitions of things like piece hashes, peers, as well
 //! as what's included in a `.torrent` file, for example.
 use crate::bencoding::Bencoding;
-use std::{convert::TryFrom, path::PathBuf, str, time::SystemTime};
+use std::{convert::TryFrom, path::PathBuf, str, time};
 
 pub enum TryFromBencodingError<'b> {
     ExpectedInt(&'b Bencoding),
     ExpectedByteString(&'b Bencoding),
     ExpectedList(&'b Bencoding),
     ExpectedDict(&'b Bencoding),
+    ExceedsSystemTime(i64),
     NotUTF8 {
         bencoding: &'b Bencoding,
         error: str::Utf8Error,
@@ -46,6 +47,40 @@ fn extract_bytes<'b>(bencoding: &'b Bencoding) -> Result<&'b [u8], TryFromBencod
 fn extract_string<'b>(bencoding: &'b Bencoding) -> Result<&'b str, TryFromBencodingError<'b>> {
     let bytes = extract_bytes(bencoding)?;
     str::from_utf8(bytes).map_err(|e| TryFromBencodingError::from_utf8_error(bencoding, e))
+}
+
+#[inline]
+fn extract_key<'b>(
+    bencoding: &'b Bencoding,
+    key: &'static [u8],
+) -> Result<&'b Bencoding, TryFromBencodingError<'b>> {
+    match bencoding {
+        Bencoding::Dict(map) => map
+            .get(key)
+            .ok_or(TryFromBencodingError::MissingKey { bencoding, key }),
+        _ => Err(TryFromBencodingError::ExpectedDict(bencoding)),
+    }
+}
+
+#[inline]
+fn extract_list<'b>(
+    bencoding: &'b Bencoding,
+) -> Result<&'b [Bencoding], TryFromBencodingError<'b>> {
+    match bencoding {
+        Bencoding::List(bx) => Ok(bx),
+        _ => Err(TryFromBencodingError::ExpectedList(bencoding)),
+    }
+}
+
+#[inline]
+fn extract_system_time<'b>(
+    bencoding: &'b Bencoding,
+) -> Result<time::SystemTime, TryFromBencodingError<'b>> {
+    let seconds = extract_int(bencoding)?;
+    let from_beginning = time::Duration::from_secs(seconds as u64);
+    time::UNIX_EPOCH
+        .checked_add(from_beginning)
+        .ok_or(TryFromBencodingError::ExceedsSystemTime(seconds))
 }
 
 /// Represents the location of some tracker.
@@ -97,12 +132,14 @@ impl<'b> TryFrom<&'b Bencoding> for TrackerAddr {
     }
 }
 
+const PIECE_HASH_SIZE: usize = 20;
+
 /// Represents the SHA1 hash of a given piece.
 ///
 /// This is how we verify the integrity of the data we receive from a torrent.
 /// For each piece, we can calculate the SHA1 hash of that piece, and compare that
 /// to the information we know about that torrent.
-pub struct PieceHash([u8; 20]);
+pub struct PieceHash([u8; PIECE_HASH_SIZE]);
 
 /// This contains the info about a specific file in this torrent.
 ///
@@ -133,7 +170,7 @@ pub struct Torrent {
     /// higher values.
     pub trackers: Box<[(u8, TrackerAddr)]>,
     /// If present, this contains the time of creation of this torrent.
-    pub creation: Option<SystemTime>,
+    pub creation: Option<time::SystemTime>,
     /// If present, this contains a message about this torrent.
     pub comment: Option<String>,
     /// If present, this contains a description of the program that created this torrent.
@@ -164,6 +201,73 @@ pub struct Torrent {
     /// This means that a piece can overlap an arbitrary number of files, and that the final
     /// piece may be a different length than the others.
     pub files: Box<[FileInfo]>,
+}
+
+pub enum ParseTorrentError<'b> {
+    Bencoding(TryFromBencodingError<'b>),
+    BadHashLength(usize),
+}
+
+impl<'b> From<TryFromBencodingError<'b>> for ParseTorrentError<'b> {
+    fn from(error: TryFromBencodingError<'b>) -> Self {
+        ParseTorrentError::Bencoding(error)
+    }
+}
+
+impl<'b> TryFrom<&'b Bencoding> for Torrent {
+    type Error = ParseTorrentError<'b>;
+
+    fn try_from(bencoding: &'b Bencoding) -> Result<Self, Self::Error> {
+        let trackers = match extract_key(bencoding, b"announce-list") {
+            Err(_) => {
+                let tracker = TrackerAddr::try_from(bencoding)?;
+                vec![(0, tracker)].into_boxed_slice()
+            }
+            Ok(inner) => {
+                let tiers = extract_list(inner)?;
+                let mut trackers = Vec::with_capacity(tiers.len());
+                for (index, tier) in tiers.iter().enumerate() {
+                    let tier_list = extract_list(tier)?;
+                    for tracker in tier_list {
+                        trackers.push((index, TrackerAddr::try_from(tracker)?))
+                    }
+                }
+                trackers.into_boxed_slice()
+            }
+        };
+        let creation = extract_key(bencoding, b"creation date")
+            .ok()
+            .map(extract_system_time)
+            .transpose()?;
+        let comment = extract_key(bencoding, b"comment")
+            .ok()
+            .map(|inner| extract_string(inner).map(String::from))
+            .transpose()?;
+        let created_by = extract_key(bencoding, b"created by")
+            .ok()
+            .map(|inner| extract_string(inner).map(String::from))
+            .transpose()?;
+        let info = extract_key(bencoding, b"info")?;
+        let private_option = extract_key(info, b"private")
+            .ok()
+            .map(extract_int)
+            .transpose()?;
+        let private = private_option.map(|x| x == 1).unwrap_or(false);
+        let piece_length = extract_int(extract_key(info, b"piece length")?)? as usize;
+        let piece_bytes = extract_bytes(extract_key(info, b"pieces")?)?;
+        let piece_bytes_len = piece_bytes.len();
+        if piece_bytes_len % PIECE_HASH_SIZE != 0 {
+            return Err(ParseTorrentError::BadHashLength(piece_bytes_len));
+        }
+        let mut piece_hashes: Vec<PieceHash> =
+            Vec::with_capacity(piece_bytes_len / PIECE_HASH_SIZE);
+        for chunk in piece_bytes.chunks_exact(PIECE_HASH_SIZE) {
+            let mut arr: [u8; PIECE_HASH_SIZE] = Default::default();
+            arr.copy_from_slice(chunk);
+            piece_hashes.push(PieceHash(arr));
+        }
+        unimplemented!()
+    }
 }
 
 #[cfg(test)]
